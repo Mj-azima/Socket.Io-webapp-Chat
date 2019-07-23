a5b72e439202ff171fffc70738168fb0103ef499
Mj <m.javad1391@gmail.com>
2019-07-23 18:05:44 +0430

    require socket.io & create event connection

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
-
+var io = require('socket.io').listen(http);
 
 
 http.listen(3000, function(){
@@ -12,3 +12,10 @@ http.listen(3000, function(){
 app.get('/', function(req , res){
     res.send('chat');
 });
+
+
+
+io.on('connection', function(socket){
+    console.log('a user connection');
+});
+
